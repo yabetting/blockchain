e4d63258e7357a4f65576ecb811a4140c3b6d688
yabetting <yabetting@gmail.com>
2017-06-16 15:13:03 +0900

    Update chaincode_example_02.go

--- sample/chaincode_example_02.go
+++ sample/chaincode_example_02.go
@@ -72,10 +72,7 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 }
 
 func main() {
-    err := shim.Start(new(SimpleChaincode))
-    if err != nil {
-        fmt.Printf("Error starting Simple chaincode: %s", err)
-    }
+
 }
 
 
